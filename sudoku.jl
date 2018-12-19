de9161c408cd16bb7f20910479fb59ed2aa223fa
Klemen Kobau <klemen.kobau@gmail.com>
2018-12-19 09:41:37 +0100

    heuristic

--- sudoku.jl
+++ sudoku.jl
@@ -1,6 +1,6 @@
 # create the colormap for a traditional sudoku, where a 9*9 board is split into 9 3*3 areas of the same colour
 function createNormalSudokuMap()
-	out = zeros(UInt8,9,9)
+	out = zeros(Int8,9,9)
 	inc = [1 1 1 2 2 2 3 3 3]
 
 	for i in [1,4,7]
@@ -44,20 +44,51 @@ end
 # creates a new sudoku based on the sudoku_map, with "filled" fields filled, (filled is a number, that many fields remain filled)
 function createSudoku(sudoku_map,filled = 5)
 	sudoku = zeros(Int8,size(sudoku_map))
-	# TODO
-	return sudoku
+	sudoku_heuristic
+
+	sudoku = solveSudokuHeuristic(sudoku,sudoku_map)[3]
+	to_leave = zeros(Int8,size(sudoku_map))
+
+	while filled > 0
+		i = rand(1:maximum(size(sudoku)))
+		j = rand(1:maximum(size(sudoku)))
+		
+		if to_leave[i,j] != 1
+			to_leave[i,j] = 1
+			filled = filled - 1
+		end
+	end
+
+	solution = copy(sudoku)
+	sudoku = sudoku .* to_leave
+
+	return (sudoku,solution)
+end
+
+function createHeuristicRandom(sudoku_map,filled = 4)
+	sudoku_heuristic = ones(Int8,size(sudoku_map))
+	while filled > 0
+		i = rand(1:maximum(size(sudoku)))
+		j = rand(1:maximum(size(sudoku)))
+		
+		if sudoku_heuristic[i,j] != 1
+			sudoku_heuristic[i,j] = 1
+			filled = filled - 1
+		end
+	end
+	return sudoku_heuristic
 end
 
 #=
 	sudoku -> the sudoku grid, can have some numbers already filled
 	sudoku_map -> the colormap for the sudoku, a color cannot have two numbers with the same value
-	sudoku_hevristic -> optional, which fields the sudoku solver prioritizes, if not provided all fields have an equal pripority, the solver will solve up to down, left to right
+	sudoku_heuristic -> optional, which fields the sudoku solver prioritizes, if not provided all fields have an equal pripority, the solver will solve up to down, left to right
 	returns (solution_found, number of steps, sudoku)
 		solution_found -> boolean, weather a solution was found or not
 		number of steps -> number of different numbers tried to arrive to the returned sudoku
 		sudoku -> the sudoku the program had stored at the end
 =#
-function solveSudokuHevristic(sudoku,sudoku_map,sudoku_hevristic = nothing)
+function solveSudokuHeuristic(sudoku,sudoku_map,sudoku_heuristic = nothing)
 	function is_zero(x)
 		if x == 0
 			return 1
@@ -65,41 +96,42 @@ function solveSudokuHevristic(sudoku,sudoku_map,sudoku_hevristic = nothing)
 		return 0
 	end
 
-	if sudoku_hevristic == nothing
-		sudoku_hevristic = map(is_zero,sudoku)
+	if sudoku_heuristic == nothing
+		sudoku_heuristic = map(is_zero,sudoku)
 	else
 		values = map(x -> x == 0,sudoku)
-		sudoku_hevristic = sudoku_hevristic .* values
+		sudoku_heuristic = sudoku_heuristic .* values
 	end
 
-	start = findmax(sudoku_hevristic)[2]
-	return solveSudokuRek(sudoku,sudoku_map,sudoku_hevristic,start,Int64(0))
+	start = findmax(sudoku_heuristic)[2]
+	return solveSudokuRek(sudoku,sudoku_map,sudoku_heuristic,start,Int64(0))
 end
 
 #=
-	helper function for function solveSudokuHevristic
+	helper function for function solveSudokuHeuristic
 	curr -> current field being filled, type CartesianIndex(i, j)
 =#
-function solveSudokuRek(sudoku,sudoku_map,sudoku_hevristic,curr,num_steps)
-
+function solveSudokuRek(sudoku,sudoku_map,sudoku_heuristic,curr,num_steps)
 	if num_steps < 0
 		@warn "overflow for number of steps, result will be inaccurate", num_steps
+	elseif num_steps > 100000
+		error("exceded 100000 steps", sudoku,sudoku_heuristic)
 	end
-
-	hevristic = sudoku_hevristic[curr]
+	
+	heuristic = sudoku_heuristic[curr]
 	for number in 1:maximum(size(sudoku))
 		num_steps += 1
 		if checkForAvailability(sudoku,sudoku_map,number,curr)
 			sudoku[curr] = number
-			sudoku_hevristic[curr] = 0
-			next = findmax(sudoku_hevristic)
+			sudoku_heuristic[curr] = 0
+			next = findmax(sudoku_heuristic)
 			
 			# solved sudoku
 			if next[1] == 0
 				return (true,num_steps,sudoku)
 			end
 			
-			solution = solveSudokuRek(sudoku,sudoku_map,sudoku_hevristic,next[2],num_steps)
+			solution = solveSudokuRek(sudoku,sudoku_map,sudoku_heuristic,next[2],num_steps)
 
 			# if we found a solution
 			if solution[1]
@@ -109,7 +141,7 @@ function solveSudokuRek(sudoku,sudoku_map,sudoku_hevristic,curr,num_steps)
 				num_steps = solution[2]
 			end
 			# cleanup if a solution was not found
-			sudoku_hevristic[curr] = hevristic
+			sudoku_heuristic[curr] = heuristic
 			sudoku[curr] = 0
 		end
 	end
@@ -118,4 +150,4 @@ end
 
 sudoku_map = createNormalSudokuMap()
 sudoku = createSudoku(sudoku_map)
-println(solveSudokuHevristic(sudoku,sudoku_map))
+println(sudoku)
